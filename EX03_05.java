84eb6a24dae6e0df0c5364f0b6d8d5da4080824f
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:29:09 +0900

    03_05

---
+++ EX03_05.java
@@ -0,0 +1,21 @@
+//読み込んだ整数値が正であれば5で割り切れるかどうかを表示、正でなければその旨を表示
+
+import java.util.Scanner;
+
+class EX03_05{
+	
+		public static void main(String[] args){
+			Scanner stdIn=new Scanner(System.in);
+			
+			System.out.print("整数値：");
+			int n=stdIn.nextInt();
+			
+			if (n > 0)
+				if(n % 5 == 0)
+					System.out.println("その値は5で割り切れます。");
+				else
+					System.out.println("その値は5で割り切れません");
+			else 
+				System.out.println("正でない値が入力されました。");
+		}
+}
