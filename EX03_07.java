70c1ba873df61446261d874f4f42bcf2c0b736ca
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:29:27 +0900

    03_07

---
+++ EX03_07.java
@@ -0,0 +1,23 @@
+//読み込んだ整数値が正であれば3で割った剰余を表示、正でなければその旨を表示
+
+import java.util.Scanner;
+
+class EX03_07{
+	
+		public static void main(String[] args){
+			Scanner stdIn=new Scanner(System.in);
+			
+			System.out.print("整数値：");
+			int n=stdIn.nextInt();
+			
+			if (n > 0)
+				if(n % 3 == 0)
+					System.out.println("その値は3で割り切れます。");
+				else if(n % 3 == 1)
+						System.out.println("その値を3で割った余りは1です。");
+				else 
+					System.out.println("その値を3で割った余りは2です。");
+			else 
+				System.out.println("正でない値が入力されました。");
+		}
+}
