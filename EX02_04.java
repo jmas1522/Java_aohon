97082d18092442a002761e9a5357736cc188abcd
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:26:47 +0900

    02_04

---
+++ EX02_04.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class EX02_04 {
+	public static void main(String[] args){
+		Scanner stdIn=new Scanner(System.in);
+		
+		System.out.print("整数値：");
+		int a= stdIn.nextInt();
+		
+		System.out.println("10を加えた値は"+(a+10)+"です。");
+		System.out.println("10を減じた値は"+(a+10)+"です。");
+	}
+}
