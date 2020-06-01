300db1949f22859497882063c826c2f5d388434c
jmas1522 <tsujams1522@gmail.com>
2020-06-01 20:49:10 +0900

    05_03

---
+++ EX05_03.java
@@ -0,0 +1,13 @@
+//論理型の変数にtrueやfalseを代入して、その値を表示するプログラム
+
+class EX05_03 {
+	
+	public static void main(String[] args){
+		
+		boolean t = true;
+		boolean f = false;
+		
+		System.out.println("true = "+ t);
+		System.out.println("false = "+ f);
+	}
+}
