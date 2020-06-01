d1bec25ec42356d495f1f4ddf6af4e3014f70d42
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:26:00 +0900

    01_02

---
+++ EX01_02.java
@@ -0,0 +1,9 @@
+class EX01_02{
+	public static void main(String[] args){
+		System.out.println("í“");
+		System.out.println("ë∫");
+		System.out.println("ìç");
+		System.out.println("â¡");
+	}
+}
+		
