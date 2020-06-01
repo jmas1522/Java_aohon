2680b9b82f483ef924bf7c9bbb90b89fe6be2010
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:27:14 +0900

    02_07

---
+++ EX02_07_a.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+class EX02_07_a {
+	public static void main(String[] args){
+		Random rand=new Random();
+		int a=rand.nextInt(10);
+		System.out.println("ˆêŒ…‚Ì³‚Ì®”’l"+a);
+	}
+}
