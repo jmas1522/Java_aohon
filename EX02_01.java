4e2a55d426cde129cb4e6704e9ea9b6fc70dccdd
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:26:19 +0900

    02_01

---
+++ EX02_01.java
@@ -0,0 +1,12 @@
+class EX02_01 {
+	public static void main(String[] args){
+		double x;
+		double y;
+		x=63.0;
+		y=18.0;
+		System.out.println("xの値は"+x+"です。");
+		System.out.println("yの値は"+y+"です。");
+		System.out.println("合計は"+(x+y)+"です。");
+		System.out.println("平均は"+(x+y)/2+"です。");
+	}
+}
