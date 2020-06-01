a31caa2e0e529e45fb55d7a930c3787573725b98
jmas1522 <tsujams1522@gmail.com>
2020-06-01 18:26:39 +0900

    02_03

---
+++ EX02_03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class EX02_03 {
+	public static void main(String[] args){
+		Scanner stdIn=new Scanner(System.in);
+		
+		System.out.print("®”’lF");
+		int a=stdIn.nextInt();
+		System.out.println(a+"‚Æ“ü—Í‚µ‚Ü‚µ‚½‚ËB");
+	}
+}
